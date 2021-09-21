877c167d6455d81b23b8ad929a6f2af83c333f59
rajthecodr23 <90940406+rajthecodr23@users.noreply.github.com>
2021-09-21 19:57:38 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+var trex ,trex_running;
+var ground
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+ trex = createSprite(50,180,20,50);
+ground = createSprite(200,180,400,20)
+ 
+}
+
+function draw(){
+  background("white")
+  if keyDown (space)
+
+}
